78a79cb060b9a0c30d72ace75cd4557867d208d9
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2024-02-29 17:42:32 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan"; // Update the name to "Susan"
+const height = 39; // Update the height to a value less than 40 and greater than 0
 const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+module.exports = { name, height, message };
